3abb053f61c454026d3057fd201a0129e8509826
Mickaël Douceré <mickamickadou@gmail.com>
2024-10-08 19:33:24 +0200

    Création du reducer utilisateur

---
+++ argent-bank/src/reducers/userReducer.js
@@ -0,0 +1,31 @@
+import {
+  EDIT_USER_INFOS,
+  GET_USER_INFOS,
+  LOG_IN,
+  OPEN_USER_EDIT_FORM,
+} from '../actions/user.actions'
+
+const initialState = {
+  loggedIn: false,
+  openedUserEditForm: false,
+  getUserInfos: '',
+}
+
+export function userReducer(state = initialState, action) {
+  switch (action.type) {
+    case LOG_IN:
+      return { ...state, loggedIn: action.payload }
+
+    case OPEN_USER_EDIT_FORM:
+      return { ...state, openedUserEditForm: action.payload }
+
+    case GET_USER_INFOS:
+      return { ...state, getUserInfos: action.payload }
+
+    case EDIT_USER_INFOS:
+      return { ...state, getUserInfos: action.payload }
+
+    default:
+      return state
+  }
+}

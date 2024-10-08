066fad52738acedea1e23be0b80a8c546ab74d0c
Mickaël Douceré <mickamickadou@gmail.com>
2024-10-08 19:18:37 +0200

    Implémentation du dashboard Html CSS et gestion des états avec Redux

--- argent-bank/src/page/dasboard/Dashboard.jsx
+++ argent-bank/src/page/dasboard/Dashboard.jsx
@@ -1,55 +1,88 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
 import './dashboard.css'
 
-function Dashboard() {
-  const userName = 'Micka'
+import EditForm from '../../components/editForm/EditForm'
 
-  return (
-    <main className="main bg-dark">
-      <div className="header">
-        {/* Créer une fonction EditFormCollapsed pour afficher ou non le formulaire d'édition en fonction de l'appui sur le bouton 'Edit Name'*/}
-        {/*  Le bouton save permet de modifier user name => modifie le store et referme le formulaire*/}
-        {/* Le bouton cancel referme le formulaire */}
-        <h1>
-          Welcome back
-          <br />
-          {userName}
-        </h1>
-        <button className="edit-button">Edit Name</button>
+import { openEditForm } from '../../actions/user.actions'
+
+function Dashboard() {
+  const user = useSelector((state) => state.user)
+
+  const dispatch = useDispatch()
+
+  const isEditFormOpened = user.openedUserEditForm
+
+  const userName = user.getUserInfos.userName
+
+  const handleOpeningStatus = () => dispatch(openEditForm(true))
+
+  // CODE pour éviter l'accès sans authorisation au dashboard quand on tape directement l'URL ou le retour en arrière dans le navigateur !
+  const navigate = useNavigate()
+  if (user.loggedIn === false) {
+    navigate('/sign-in')
+    return (
+      <div className="redirection">
+        <h1>Vous devez être connecté pour accéder à cette page !</h1>
+        <h2>
+          Veuillez cliquer sur le lien "Sign in" en haut à gauche pour vous
+          authentifiez ou cliquer sur le logo pour revenir à la page d'accueil.
+        </h2>
       </div>
-      <section className="account-content">
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-            <p className="account-amount">$2,082.79</p>
-            <p className="account-amount-description">Available Balance</p>
+    )
+  } else {
+    return (
+      <main className="main bg-dark">
+        {isEditFormOpened === false ? (
+          <div className="header">
+            <h1>
+              Welcome back
+              <br />
+              {userName} !
+            </h1>
+            <button className="edit-button" onClick={handleOpeningStatus}>
+              Edit Name
+            </button>
           </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
+        ) : (
+          <EditForm />
+        )}
+        <section className="account-content">
+          <section className="account">
+            <div className="account-content-wrapper">
+              <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+              <p className="account-amount">$2,082.79</p>
+              <p className="account-amount-description">Available Balance</p>
+            </div>
+            <div className="account-content-wrapper cta">
+              <button className="transaction-button">View transactions</button>
+            </div>
+          </section>
+          <section className="account">
+            <div className="account-content-wrapper">
+              <h3 className="account-title">Argent Bank Savings (x6712)</h3>
+              <p className="account-amount">$10,928.42</p>
+              <p className="account-amount-description">Available Balance</p>
+            </div>
+            <div className="account-content-wrapper cta">
+              <button className="transaction-button">View transactions</button>
+            </div>
+          </section>
+          <section className="account">
+            <div className="account-content-wrapper">
+              <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
+              <p className="account-amount">$184.30</p>
+              <p className="account-amount-description">Current Balance</p>
+            </div>
+            <div className="account-content-wrapper cta">
+              <button className="transaction-button">View transactions</button>
+            </div>
+          </section>
         </section>
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-            <p className="account-amount">$10,928.42</p>
-            <p className="account-amount-description">Available Balance</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
-        </section>
-        <section className="account">
-          <div className="account-content-wrapper">
-            <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-            <p className="account-amount">$184.30</p>
-            <p className="account-amount-description">Current Balance</p>
-          </div>
-          <div className="account-content-wrapper cta">
-            <button className="transaction-button">View transactions</button>
-          </div>
-        </section>
-      </section>
-    </main>
-  )
+      </main>
+    )
+  }
 }
 
 export default Dashboard

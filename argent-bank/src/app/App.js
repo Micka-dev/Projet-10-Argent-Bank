a763225c97a76a595458272e8d09f4e13bdcd2ef
Mickaël Douceré <mickamickadou@gmail.com>
2024-10-08 19:20:44 +0200

    Mise en place du provider avec le store en props

--- argent-bank/src/app/App.js
+++ argent-bank/src/app/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-// import { Provider } from 'react-redux'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-// import { store } from './store'
+import { store } from '../store/store.js'
 
 import Router from '../routers/router/Router.jsx'
 import Header from '../layout/header/Header.jsx'
@@ -9,14 +9,13 @@ import Footer from '../layout/footer/Footer.jsx'
 
 function App() {
   return (
-    // <Provider store={store}>
-    <BrowserRouter>
-      <Header />
-      <Router />
-      <Footer />
-    </BrowserRouter>
-    // </Provider>
-    // <div>hello</div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Router />
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
